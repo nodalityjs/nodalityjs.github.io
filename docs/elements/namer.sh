42e62a6bc9bf8181e66da6555c8ca59204ce38e5
filipvabrousek <filipvabrousek1@gmail.com>
2025-06-27 21:21:46 +0200

    Hello

--- docs/elements/namer.sh
+++
@@ -1,7 +0,0 @@
-# namer.sh
-for file in *; do
-    if [ -f "$file" ]; then
-        title="${file%.md}"  # Remove the '.md' extension from the filename
-        echo "# $title" | cat - "$file" > temp && mv temp "$file"
-    fi
-done
