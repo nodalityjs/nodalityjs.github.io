6ea2a63d0d31a42cd3a8a0db19add7efa810ec9d
filipvabrousek <filipvabrousek1@gmail.com>
2025-08-26 00:44:36 +0200

    Auto update

---
+++ assets/files/namer-d8041ff8248b1e238f698a8bf8194aab.sh
@@ -0,0 +1,8 @@
+# namer.sh
+# namer.sh
+for file in *; do
+    if [ -f "$file" ]; then
+        title="${file%.md}"  # Remove the '.md' extension from the filename
+        echo "# $title" | cat - "$file" > temp && mv temp "$file"
+    fi
+done
